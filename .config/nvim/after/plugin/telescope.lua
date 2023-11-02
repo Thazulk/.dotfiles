638d14704cc1e4c1bad3823b72017590bb44091b
Szedmák Péter <suedmakp@digitlean.com>
2023-11-02 16:31:27 +0100

    minor mods

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -41,7 +41,7 @@ require('telescope').setup {
 }
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_commits, {})
 vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>pg', builtin.live_grep, {});
 vim.keymap.set('n', '<leader>ps', function()

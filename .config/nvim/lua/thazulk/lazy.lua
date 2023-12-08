e9dcc91b24d8bbecc5bc00500401d96edbc2140d
Péter Szedmák <thazulk@gmail.com>
2023-12-08 11:53:44 +0100

    neogit added

--- .config/nvim/lua/thazulk/lazy.lua
+++ .config/nvim/lua/thazulk/lazy.lua
@@ -207,6 +207,16 @@ local plugins = {
         lazy = false,
         priority = 1000,
         opts = {},
+    },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+            "nvim-telescope/telescope.nvim", -- optional
+            "sindrets/diffview.nvim", -- optional
+            "ibhagwan/fzf-lua",      -- optional
+        },
+        config = true
     }
 
 }

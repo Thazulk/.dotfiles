d72dd8d312e368d9323ba7a3f73b75d2462015f1
Péter Szedmák <thazulk@gmail.com>
2024-01-01 21:22:41 +0100

    conform plugin fixed, tmux popup fzf session and windows searching

--- .config/nvim/after/plugin/conform.lua
+++ .config/nvim/after/plugin/conform.lua
@@ -9,6 +9,7 @@ conform.setup({
         lua = { "stylua" },
         -- Conform will run multiple formatters sequentially
         python = { "isort", "black" },
+        vue = { "prettierd", "prettier" },
         -- Use a sub-list to run only the first available formatter
         javascript = { { "prettierd", "prettier" } },
         bash = { "shfmt" },

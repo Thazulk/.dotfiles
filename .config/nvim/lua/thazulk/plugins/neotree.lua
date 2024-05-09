3e630056bf56a06439ed19284c97dd52950407ee
Peter Szedmak <thazulk@gmail.com>
2024-05-09 12:51:13 +0200

    added pnpm path. neotree duplicate keymap removed

--- .config/nvim/lua/thazulk/plugins/neotree.lua
+++ .config/nvim/lua/thazulk/plugins/neotree.lua
@@ -80,20 +80,6 @@ return {
 			})
 		end,
 		keys = {
-			{
-				"<leader>fe",
-				function()
-					require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-				end,
-				desc = "Explorer NeoTree (root dir)",
-			},
-			{
-				"<leader>fE",
-				function()
-					require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-				end,
-				desc = "Explorer NeoTree (cwd)",
-			},
 			{ "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
 			{ "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
 			{
--- .zshrc
+++ .zshrc
@@ -143,3 +143,11 @@ eval "$(pyenv init -)"
 
 # Generated for envman. Do not edit.
 [ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
+
+# pnpm
+export PNPM_HOME="/home/pszedmak/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end

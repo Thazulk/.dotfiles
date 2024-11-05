38914c0ba24674474ac9259a9251b03034eef801
Thazulk <thazulk@gmail.com>
2024-11-05 09:10:40 +0100

    wallpaper

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,9 @@
-local wezterm = require("wezterm")
+local wezterm = require("wezterm") --[[@as Wezterm]]
+local config = wezterm.config_builder()
+wezterm.log_info("reloading")
 local act = wezterm.action
 
-local config = wezterm.config_builder()
+require("links").setup(config)
 
 config.color_scheme = "Tokyo Night Moon"
 
@@ -14,7 +16,7 @@ config.window_padding = {
 
 config.hide_tab_bar_if_only_one_tab = true
 
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("JetBrains Mono Nerd")
 
 config.font = wezterm.font_with_fallback({
 	-- "FiraCode Nerd Font Mono",
@@ -22,7 +24,8 @@ config.font = wezterm.font_with_fallback({
 	-- "DejaVu Sans Mono",
 	-- "Monospace",
 })
-config.font_size = 11
+config.font_size = 10
 config.allow_win32_input_mode = false
 config.keys = { { key = " ", mods = "CTRL", action = act.SendKey({ key = " ", mods = "CTRL" }) } }
+config.window_background_opacity = 0.95
 return config
